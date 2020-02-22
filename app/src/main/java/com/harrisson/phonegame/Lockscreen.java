fe0071e9efd6802d0956857fd79489dbe3885e14
Harrisson Pereira <pereiraharrisson@gmail.com>
2020-02-22 14:12:20 -0300

    base structure

---
+++ app/src/main/java/com/harrisson/phonegame/Lockscreen.java
@@ -0,0 +1,14 @@
+package com.harrisson.phonegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lockscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lockscreen);
+    }
+}
